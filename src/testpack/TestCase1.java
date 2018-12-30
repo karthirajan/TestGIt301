6da22516a2b4fc99aaa89bebc4f53c4830d7f9b7
teamlead11 <automationlead11@gmail.com>
2018-12-30 09:36:05 +0530

    modified test case1

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -14,4 +14,8 @@ public class TestCase1 {
 	public static void add(int a, int b, int c) {
 		System.out.println(a + b + c);
 	}
+
+	public static void add(String a, String b) {
+		System.out.println(a + b);
+	}
 }
