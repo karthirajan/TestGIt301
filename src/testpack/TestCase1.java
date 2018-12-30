8e572598c2e16e9343c2d4439cb8a66005b88888
teamlead11 <automationlead11@gmail.com>
2018-12-30 09:53:16 +0530

    demo conflicts by user2

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -10,4 +10,9 @@ public class TestCase1 {
 	public static void add(int a, int b) {
 		System.out.println(a + b);
 	}
+	
+	public static void add(float a, float b) {
+		System.out.println(a + b);
+	}
+
 }
