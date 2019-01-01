1377668f7437bee3a0f9d01deb02981bc39e990a
SeethaRamalingam <seetha.3999@gmail.com>
2019-01-01 14:33:12 +0530

    SortedArray_seetha

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -229,6 +229,30 @@ public void arraySortBySiva() {
 		}
 	}
 	
+	
+	public static void ArraySort_Seetha()
+	{
+		int a[] = {50,10,25,55,80,20},temp;
+		
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]>a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		System.out.println("After Sorting:");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+	
 	}
   
     
